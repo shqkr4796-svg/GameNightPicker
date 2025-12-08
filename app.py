433175404ae4291c5e33ec17aa3cf4531c0ef472
shqkr4796-svg <shqkr4796@gmail.com>
2025-12-08 05:39:55 +0000

    ok

--- app.py
+++ app.py
@@ -11,3 +11,6 @@ app.secret_key = os.environ.get("SESSION_SECRET", "life-sim-secret-key-2024")
 # Import routes after app creation
 from routes import *
 
+if __name__ == "__main__":
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host="0.0.0.0", port=port)
